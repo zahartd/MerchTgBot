7726af0c15a4facd20d40780098101989e179e59
Zaharakatd <37711267+Zaharakatd22@users.noreply.github.com>
2021-02-04 20:36:07 +0400

    Last commit

--- handlers/users/edit_basket.py
+++ handlers/users/edit_basket.py
@@ -1,8 +1,16 @@
-from aiogram import types
-from aiogram.dispatcher.filters.builtin import Command
+import logging
 
-from handlers.users import show_goods_basket
+from aiogram import types
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.builtin import Command
+from aiogram.types import ReplyKeyboardRemove
+
+from data import poll_questions
+from handlers.users import show_goods_basket, ask_user, get_user_data, show_main_menu
+from keyboards.default import edit_basket_menu, position_keyboard
 from loader import dp
+from states import EditBasketDeletePos, EditBasketEditPos
+from utils.db_api.db_commands import get_basket_size, get_user_database_data, update_user_data
 
 
 # Edit current user basket
@@ -10,5 +18,114 @@ from loader import dp
 @dp.message_handler(text='Редактировать корзину ✏')
 @dp.message_handler(Command('edit_basket'))
 async def edit_basket(message: types.Message):
-    await message.answer(text=f'Эта функция скоро заработает')
+    await message.answer(text=f'Вы вошли в режим редакирования', reply_markup=edit_basket_menu)
+
+
+@dp.message_handler(text='Выйти 🔙')
+async def back(message: types.Message):
     await show_goods_basket(message)
+
+
+@dp.message_handler(text='Очистить корзину 🎇')
+async def clear_basket(message: types.Message):
+    user_id: int = message.from_user.id
+
+    await update_user_data(user_id, basket_name=[], basket_count=[], basket_item_price=[], total_price=0)
+    await message.answer(text=f'Корзина очищена 🎇')
+    await show_main_menu(message)
+
+
+@dp.message_handler(text='Удалить позицию ❌')
+async def ask_position(message: types.Message):
+    user_id: int = message.from_user.id
+    basket_size: int = await get_basket_size(user_id)
+    await ask_user(message=message, state=EditBasketDeletePos, text=poll_questions['goods_position'],
+                   markup=position_keyboard(pos_cnt=basket_size))
+
+
+@dp.message_handler(text='Редактировать позицию ✏')
+async def ask_position(message: types.Message):
+    user_id: int = message.from_user.id
+    basket_size: int = await get_basket_size(user_id)
+    await ask_user(message=message, state=EditBasketEditPos, text=poll_questions['goods_position'],
+                   markup=position_keyboard(pos_cnt=basket_size))
+
+
+@dp.message_handler(state=EditBasketEditPos.Position)
+async def ask_count(message: types.Message, state: FSMContext):
+    markup = ReplyKeyboardRemove()
+    await get_user_data(message=message, main_state=state, data_name='goods_position', update_db=False)
+    await ask_user(message=message, state=EditBasketEditPos, text=poll_questions['item_count'], markup=markup)
+
+
+@dp.message_handler(state=EditBasketDeletePos.Position)
+async def delete_position(message: types.Message, state: FSMContext):
+    user_id: int = message.from_user.id
+    await get_user_data(message=message, main_state=state, data_name='goods_position', update_db=False)
+
+    async with state.proxy() as data:
+        is_valid: bool = data['goods_position'].isdigit()
+
+        if is_valid:
+            basket_count: list[int] = await get_user_database_data(user_id=user_id, data_name='basket_count')
+            basket_name: list[str] = await get_user_database_data(user_id=user_id, data_name='basket_name')
+            basket_item_price: list[int] = await get_user_database_data(user_id=user_id, data_name='basket_item_price')
+            total_price: int = await get_user_database_data(user_id=user_id, data_name='total_price')
+
+            new_basket_name: list[str] = []
+            new_basket_count: list[int] = []
+            new_basket_item_price: list[int] = []
+
+            for i, basket_elm in enumerate(zip(basket_name, basket_count, basket_item_price), start=1):
+                basket_name_elm = basket_elm[0]
+                basket_count_elm = basket_elm[1]
+                basket_item_price_elm = basket_elm[2]
+                if i != int(data['goods_position']):
+                    new_basket_name.append(basket_name_elm)
+                    new_basket_count.append(basket_count_elm)
+                    new_basket_item_price.append(basket_item_price_elm)
+                    logging.info('ffffffffffffffffffffffff', basket_name_elm, basket_count_elm, basket_item_price_elm)
+                else:
+                    total_price -= basket_item_price_elm * basket_count_elm
+
+            logging.info('iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii', new_basket_item_price)
+            await update_user_data(user_id, basket_name=new_basket_name, basket_count=new_basket_count,
+                                   basket_item_price=new_basket_item_price, total_price=total_price)
+
+            await state.finish()
+            if total_price == 0:
+                await show_goods_basket(message)
+            else:
+                await show_goods_basket(message, mode='edit')
+        else:
+            await EditBasketDeletePos.previous()
+            await message.answer(text=f'Упс( Похоже вы ввели не число.')
+            await ask_position(message)
+
+
+@dp.message_handler(state=EditBasketEditPos.Count)
+async def edit_position(message: types.Message, state: FSMContext):
+    user_id: int = message.from_user.id
+    await get_user_data(message=message, main_state=state, data_name='item_count', update_db=False)
+
+    async with state.proxy() as data:
+        is_valid: bool = data['item_count'].isdigit() and 1 <= int(data['item_count']) <= 100
+
+        if is_valid:
+            item_pos: int = int(data['goods_position']) - 1
+            basket_item_price: list[int] = await get_user_database_data(user_id=user_id, data_name='basket_item_price')
+            new_total_price: int = await get_user_database_data(user_id=user_id, data_name='total_price')
+            new_basket_count: list[int] = await get_user_database_data(user_id=user_id, data_name='basket_count')
+
+            new_total_price -= basket_item_price[item_pos] * new_basket_count[item_pos]
+            new_basket_count[item_pos] = int(data['item_count'])
+            new_total_price += basket_item_price[item_pos] * new_basket_count[item_pos]
+
+            await update_user_data(user_id, basket_count=new_basket_count, total_price=new_total_price)
+
+            await state.finish()
+            await show_goods_basket(message, mode='edit')
+        else:
+            await EditBasketEditPos.previous()
+            await message.answer(text=f'Введите пожалуйста число в интервале от 1 до 100')
+            await ask_count(message)
--- handlers/users/merch.py
+++ handlers/users/merch.py
@@ -1,9 +1,10 @@
 from typing import Union
 from aiogram.dispatcher.filters import Command
 from aiogram import types
+from aiogram.types import MediaGroup
 
 from handlers.users import show_main_menu
-from keyboards.inline.merch_keyboards import items_keyboard, menu_cd, item_keyboard
+from keyboards.inline.merch_keyboards import items_keyboard, menu_cd, item_keyboard, categories_keyboard
 from loader import dp
 from utils.db_api.db_commands import get_item
 
@@ -21,7 +22,7 @@ async def show_catalog(message: types.Message):
     await show_main_menu(message)
 
     # Show list of merch
-    await list_items(message)
+    await list_categories(message)
 
 
 # Show the main menu
@@ -31,36 +32,47 @@ async def to_main_menu(callback: types.CallbackQuery, **kwargs):
 
 
 # Show list of buttons of merch items
-async def list_items(message: Union[types.CallbackQuery, types.Message], **kwargs):
+async def list_categories(message: Union[types.CallbackQuery, types.Message], **kwargs):
     # Get keyboard
-    markup = await items_keyboard()
+    markup = await categories_keyboard()
 
     # Check what kind of update it is. If Message - send a new message
     if isinstance(message, types.Message):
-        await message.answer(text=f'Выберете мерч из списка: ', reply_markup=markup)
+        await message.answer(text=f'Смотри, что у нас есть: ', reply_markup=markup)
 
     # If CallbackQuery - change this message
     elif isinstance(message, types.CallbackQuery):
         call = message
-        await call.message.delete()
-        await call.message.answer(text=f'Выберете мерч из списка:', reply_markup=markup)
+        await call.message.edit_reply_markup(markup)
+
+
+# Show list of buttons of merch items
+async def list_items(callback: types.CallbackQuery, category: str, **kwargs):
+    # Get keyboard
+    markup = await items_keyboard(category)
+
+    await callback.message.delete()
+    await callback.message.answer(text=f'Выберете мерч из списка:', reply_markup=markup)
 
 
 # Show the item info, photo, buy button
-async def show_item(callback: types.CallbackQuery, item_id):
+async def show_item(callback: types.CallbackQuery, category, item_id):
     # Get keyboard
-    markup = item_keyboard(item_id)
+    markup = item_keyboard(category, item_id)
+    album = MediaGroup()
 
     # Delete list of items message
     await callback.message.delete()
 
     # Take a record about our item from the database
     item: Item = await get_item(item_id)
+    photos = item.photos
 
-    # Show photo, info and buy button
-    await callback.message.answer_photo(item.photos[0],
-                                        caption=f'{item.name}\nЦена: {item.price} руб.',
-                                        reply_markup=markup)
+    for photo in photos:
+        album.attach_photo(photo)
+
+    await callback.message.answer_media_group(media=album)
+    await callback.message.answer(text=f'{item.name}\nЦена: {item.price} руб.', reply_markup=markup)
 
 
 # Merch list menu navigation
@@ -74,18 +86,21 @@ async def navigate(call: types.CallbackQuery, callback_data: dict):
     # Get the current menu level that the user requested
     current_level: str = callback_data.get('level')
 
+    category = callback_data.get('category')
+
     # Get the ID of the item that the user has chosen (It is NOT ALWAYS transmitted - it can be 0)
     item_id: int = int(callback_data.get('item_id'))
 
     # Register the "levels" in which new buttons will be sent to the user
     levels: dict = {
         '0': to_main_menu,  # Go to main menu
-        '1': list_items,  # Give goods
-        '2': show_item  # Give item
+        '1': list_categories,  # Give categories
+        '2': list_items,  # Give goods
+        '3': show_item  # Give item
     }
 
     # Take away the desired function for the selected level
     current_level_function = levels[current_level]
 
     # Perform the desired function and pass the parameters received from the button there
-    await current_level_function(call, item_id=item_id)
+    await current_level_function(call, category=category, item_id=item_id)
